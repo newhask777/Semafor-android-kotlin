3e8d1a0a562980b14d8786889a1c767771fe65dd
newhask777 <newhaskellisp@gmail.com>
2024-10-16 10:39:56 +0300

    semafor first android kotlin project

---
+++ app/src/main/java/com/example/semafor/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.semafor
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    var imSemafor: ImageView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        imSemafor = findViewById(R.id.imageViewSemafor)
+    }
+
+    fun onClickStartStop(view: View)
+    {
+        view as ImageButton
+        view.setImageResource(R.drawable.red)
+    }
+}
