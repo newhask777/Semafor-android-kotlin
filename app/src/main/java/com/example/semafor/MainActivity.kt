e24145ebfb9fbe52fc0e6a3ed9367b56cc01bb29
newhask777 <newhaskellisp@gmail.com>
2024-10-16 11:48:20 +0300

    project is cook

--- app/src/main/java/com/example/semafor/MainActivity.kt
+++ app/src/main/java/com/example/semafor/MainActivity.kt
@@ -8,10 +8,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.delay
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
 
     var imSemafor: ImageView? = null
+    var counter: Int = 0
+    var timer: Timer? = null
+    var is_run: Boolean = false
+    var imageArray: IntArray = intArrayOf(R.drawable.red, R.drawable.yellow, R.drawable.green)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +31,39 @@ class MainActivity : AppCompatActivity() {
         }
 
         imSemafor = findViewById(R.id.imageViewSemafor)
+        //imSemafor?.setImageResource(imageArray[2])
     }
 
     fun onClickStartStop(view: View)
     {
         view as ImageButton
-        view.setImageResource(R.drawable.red)
+        if(!is_run)
+        {
+            startStop()
+            view.setImageResource(R.drawable.red)
+            is_run = true
+
+        }
+        else
+        {
+            imSemafor?.setImageResource(R.drawable.gray)
+            view.setImageResource(R.drawable.green)
+            timer?.cancel()
+            is_run = false
+        }
+
+    }
+
+    fun startStop()
+    {
+        timer = Timer()
+        timer?.schedule(object :TimerTask(){
+            override fun run() {
+                imSemafor?.setImageResource(imageArray[counter])
+                counter++
+                if (counter == 3) counter = 0
+            }
+
+        }, 1000)
     }
 }
